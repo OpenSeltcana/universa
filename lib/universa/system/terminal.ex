f0be8ef0a833d6fb43b97331b7756ed237d7ee9b
faztsquirrel <redisconnect@gmail.com>
2018-02-28 00:22:29 +0100

    Added better color code support.

--- lib/universa/system/terminal.ex
+++ lib/universa/system/terminal.ex
@@ -3,24 +3,39 @@ defmodule Universa.System.Terminal do
 
   @compile :nowarn_unused_vars
 
+  @fg [
+    black:   "\x1B[30m",
+    red:     "\x1B[31m",
+    green:   "\x1B[32m",
+    yellow:  "\x1B[33m",
+    blue:    "\x1B[34m",
+    purple:  "\x1B[35m",
+    cyan:    "\x1B[36m",
+    white:   "\x1B[37m",
+    reset:   "\x1B[39m",
+  ]
+  @bg [
+    black:   "\x1B[40m",
+    red:     "\x1B[41m",
+    green:   "\x1B[42m",
+    yellow:  "\x1B[43m",
+    blue:    "\x1B[44m",
+    purple:  "\x1B[45m",
+    cyan:    "\x1B[46m",
+    white:   "\x1B[47m",
+    reset:   "\x1B[49m",
+  ]
+
   capability :terminal_message, [Universa.Component.Terminal]
   def handle({:terminal_message, terminal, message}) do
     case Universa.Component.get_value(terminal) do
       # If its a socket listener, send it to templating and then the port
       {:socket, socket} ->
-        IO.inspect(message)
         :gen_tcp.send(socket,
-                      apply(Universa.Template, message[:type], [message])
-                      |> String.replace("\n", "\r\n")
-                      |> String.replace("#1#", "\x1B[30m")
-                      |> String.replace("#2#", "\x1B[31m")
-                      |> String.replace("#3#", "\x1B[32m")
-                      |> String.replace("#4#", "\x1B[33m")
-                      |> String.replace("#5#", "\x1B[34m")
-                      |> String.replace("#6#", "\x1B[35m")
-                      |> String.replace("#7#", "\x1B[36m")
-                      |> String.replace("#8#", "\x1B[37m")
-                      |> String.replace("#r#", "\x1B[37m"))
+                      apply(Universa.Template, message[:type], [message,
+                                                                @fg,
+                                                                @bg])
+                      |> String.replace("\n", "\r\n"))
       _ -> false # Do nothing
     end
   end
